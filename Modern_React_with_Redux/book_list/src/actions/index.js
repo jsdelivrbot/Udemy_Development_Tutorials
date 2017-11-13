c38076d14e2f3bc401e082b1902f03778d96d645
Antimony51122 <zhuyaosheng51122@gmail.com>
2017-11-13 20:50:27 +0000

    carrying with redux

--- Modern_React_with_Redux/book_list/src/actions/index.js
+++ Modern_React_with_Redux/book_list/src/actions/index.js
@@ -1,8 +1,8 @@
 export function selectBook(book) {
-  // selectBook is an ActionCreator, it needs to return an action,
-  // an object with a type property.
-  return {
-    type: "BOOK_SELECTED",
-    payload: book
-  };
+    // selectBook is an ActionCreator, it needs to return an action, an object with a type property.
+    // return {
+    //   type: "BOOK_SELECTED",
+    //   payload: book
+    // };
+    console.log('A book has bee selected:', book.title);
 }
