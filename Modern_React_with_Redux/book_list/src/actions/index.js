11b10db92c1687b43a3351002fa15a4a2c4798db
Antimony51122 <zhuyaosheng51122@gmail.com>
2017-11-13 22:29:54 +0000

    notes taken to action and action creators

--- Modern_React_with_Redux/book_list/src/actions/index.js
+++ Modern_React_with_Redux/book_list/src/actions/index.js
@@ -1,8 +1,7 @@
 export function selectBook(book) {
     // selectBook is an ActionCreator, it needs to return an action, an object with a type property.
-    // return {
-    //   type: "BOOK_SELECTED",
-    //   payload: book
-    // };
-    console.log('A book has bee selected:', book.title);
+    return {
+      type: "BOOK_SELECTED",
+      payload: book
+    };
 }
